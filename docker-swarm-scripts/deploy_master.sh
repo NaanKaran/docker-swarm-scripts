3f4af0e8f33c64b59eedbbd57cded7f146d3ba79
Kaarunakaran <karankaruna.mca@gmail.com>
2025-03-03 15:27:12 +0530

    vault added

--- docker-swarm-scripts/deploy_master.sh
+++ docker-swarm-scripts/deploy_master.sh
@@ -8,16 +8,16 @@ echo "Creating shared network..."
 docker network create --driver overlay --attachable shared_internal_network 2>/dev/null || echo "Network already created."
 
 # Deploy individual services
-# ./deploy_portainer.sh
-./deploy_monitoring.sh
-./deploy_mongo.sh
-./deploy_minio.sh
-./deploy_rabbitmq.sh
-./deploy_redis.sh
-./deploy_backend_api.sh
-./deploy_frontend_ui.sh
-./deploy_functions.sh
-./deploy_nginx.sh
-
+./deploy_portainer.sh
+#./deploy_monitoring.sh
+#./deploy_rabbitmq.sh
+#./deploy_redis.sh
+# ./deploy_mongo.sh
+# ./deploy_minio.sh
+# ./deploy_backend_api.sh
+# ./deploy_frontend_ui.sh
+# ./deploy_functions.sh
+# ./deploy_nginx.sh
+./deploy_vault.sh
 echo "All services deployed!"
 docker service ls
