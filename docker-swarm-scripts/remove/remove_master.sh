27af53b07e9840e51a1d9afdd8edcb8119e1e400
Kaarunakaran <karankaruna.mca@gmail.com>
2025-04-07 18:28:33 +0530

    uncommented

--- docker-swarm-scripts/remove/remove_master.sh
+++ docker-swarm-scripts/remove/remove_master.sh
@@ -2,12 +2,12 @@
 
 echo "Removing all services..."
 
-# ./remove_portainer.sh
-# ./remove_mongo.sh
-# ./remove_rabbitmq.sh
-# ./remove_monitoring.sh
-# ./remove_redis.sh
-# ./remove_minio.sh
+./remove_portainer.sh
+./remove_mongo.sh
+./remove_rabbitmq.sh
+./remove_monitoring.sh
+./remove_redis.sh
+./remove_minio.sh
 ./remove_backend_api.sh
 ./remove_frontend_ui.sh
 ./remove_functions.sh
